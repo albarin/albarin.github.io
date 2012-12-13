b8150088262ee5bba3ac14d9344405e0d446e345
Alba <albarinm@gmail.com>
2012-12-13 10:25:40 +0100

    yujuuuuuuu

--- js/functions.js
+++ js/functions.js
@@ -10,6 +10,19 @@ $(document).ready(function() {
   else {
     console.log('Not supported!');
   }
+
+  $("#canvas-stream").click(function(e) {
+    var canvas = document.getElementById('canvas-stream');
+    var ctx = canvas.getContext('2d');
+    var canvasOffset = $(this).offset();
+    var canvasX = Math.floor(e.pageX - canvasOffset.left);
+    var canvasY = Math.floor(e.pageY - canvasOffset.top);
+    var imageData = ctx.getImageData(canvasX, canvasY, 1, 1);
+    var pixel = imageData.data;
+
+    var rgb = rgbToHex(pixel[0], pixel[1], pixel[2]);
+    $("#color").css("background-color", rgb);
+  });
 });
 
 function onSuccess(stream) {
@@ -27,3 +40,7 @@ function onSuccess(stream) {
 function onError(error) {
   console.log('Error!', error);
 };
+
+function rgbToHex(r, g, b) {
+  return "#" + ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1);
+}
